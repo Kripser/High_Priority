23a31df1a3766f78071c6530a643460d8852bba1
KristerSor <krister.sorstrand@gmail.com>
2026-02-25 21:52:10 +0100

    Chef and Guest NPC Roles added

--- Scripts/NPC.gd
+++ Scripts/NPC.gd
@@ -1,3 +1,14 @@
 extends CharacterBody3D
 
-# Placeholder NPC - behavior to be added later
+@export var npc_type: String = ""
+
+func _ready():
+	var mat = StandardMaterial3D.new()
+	match npc_type:
+		"Guest":
+			mat.albedo_color = Color(1.0, 0.6, 0.8)
+		"Chef":
+			mat.albedo_color = Color(1.0, 1.0, 1.0)
+		_:
+			mat.albedo_color = Color(0.5, 0.5, 0.5)
+	$MeshInstance3D.material_override = mat
