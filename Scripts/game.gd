229de686c63c8cca05220db35fb89d70e192c3ef
KristerSor <krister.sorstrand@gmail.com>
2026-02-25 16:23:03 +0100

    test 2

--- Scripts/game.gd
+++ Scripts/game.gd
@@ -17,11 +17,21 @@ func _ready():
 	#spawn players
 	if SteamManager.is_host:
 		SteamManager.start_as_host()
-		await get_tree().create_timer(0.5).timeout
-		_spawn_players()
+		multiplayer.peer_connected.connect(_on_peer_connected)
+		
+	if not SteamManager.is_host:
+		SteamManager.start_as_client()
 		
 	SteamManager.connect("player_left", Callable(self, "_on_player_left"))
 	
+var connected_peers = []
+func _on_peer_connected(peer_id: int):
+	connected_peers.append(peer_id)
+	print("Peer connected: ", peer_id, " total: ", connected_peers.size())
+	# Spawn when all non-host players have connected
+	if connected_peers.size() >= SteamManager.lobby_members.size() - 1:
+		_spawn_players()
+
 func _spawn_players():
 	var spawn_points = $MapContainer.get_child(0).get_node("SpawnPoints").get_children()
 	
